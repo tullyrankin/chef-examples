eca30aca108307371e248c4fcf97a8a65d33cdbf
John Doe] <jdoe@example.com>
2015-02-19 23:15:43 +0000

    Added motd default recipe

--- cookbooks/motd/recipes/default.rb
+++ cookbooks/motd/recipes/default.rb
@@ -3,3 +3,10 @@
 # Recipe:: default
 #
 # Copyright (c) 2015 The Authors, All Rights Reserved.
+template "/etc/motd" do
+	action :create
+	source "motd.erb"
+	mode "0644"
+	owner "root"
+	group "root"
+end

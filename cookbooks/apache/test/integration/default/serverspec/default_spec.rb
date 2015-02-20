24b4b6ed2e61ef1d00e58981023e71af9200bfaa
Tully Rankin <TullyRankin@gmail.com>
2015-02-19 17:07:29 -0800

    Adding cookbook examples.

--- cookbooks/apache/test/integration/default/serverspec/default_spec.rb
+++ cookbooks/apache/test/integration/default/serverspec/default_spec.rb
@@ -1,12 +1,25 @@
-require 'spec_helper'
+require 'serverspec'
+set :backend, :exec
 
-describe 'apache::default' do
+describe "apache" do
+	it "is awesome" do
+		expect(true).to eq true
+	end
 
-  # Serverspec examples can be found at
-  # http://serverspec.org/resource_types.html
-  
-  it 'does something' do
-    skip 'Replace this with meaningful tests'
-  end
+	it "is installed" do
+		expect(package("httpd")).to be_installed
+	end
+
+	describe service("httpd") do
+		it { should be_enabled }
+	end
+
+	it "should be listening on port 80" do
+		expect(port(80)).to be_listening
+	end
+
+	it "displays hello" do
+		expect(command("curl localhost").stdout).to match /CentOS/
+	end
 
 end
